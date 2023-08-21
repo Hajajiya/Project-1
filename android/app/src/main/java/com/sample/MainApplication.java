9e589363d99a460d47f47de033bb92cb1d989051
Mohamed <mohamed.h@proglint.com>
2023-08-21 15:40:19 +0530

    Xml file changes in string.xml

--- android/app/src/main/java/com/sample/MainApplication.java
+++ android/app/src/main/java/com/sample/MainApplication.java
@@ -3,6 +3,7 @@ package com.sample;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush; 
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -27,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
           // packages.add(new MyReactNativePackage());
           return packages;
         }
-
+      @Override protected String getJSBundleFile() { return 
+        CodePush.getJSBundleFile(); } 
         @Override
         protected String getJSMainModuleName() {
           return "index";
