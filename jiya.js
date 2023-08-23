9e94f02e26cc2b884af2b1b92d8ed4534e48c983
Mohamed <mohamed.h@proglint.com>
2023-08-23 11:51:10 +0530

    key changes in xml file

--- jiya.js
+++ jiya.js
@@ -274,7 +274,7 @@ export class Myapp extends Component {
     return (
       <View
         style={{
-          backgroundColor: 'blue',
+          backgroundColor: 'black',
           height: 60,
           width: 70,
           marginTop: 10,
